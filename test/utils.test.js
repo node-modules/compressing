04feafa6a290d877044ed162ca4c7dcdc5e54e87
fengmk2 <fengmk2@gmail.com>
2018-08-13 15:46:10 +0800

    feat: support absolute path zip file (#10)
    try to transfer absolute and relative path to
    an validate filepath.
    
    e.g.:
    
    - /foo/ => foo/
    - ../../../foo/ => foo/

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const utils = require('../lib/utils');
 
 describe('test/utils.test.js', () => {
-  describe('utils.sourceType()', () => {
+  describe('.sourceType()', () => {
     it('should throw error', () => {
       let err;
       try {
@@ -16,7 +16,7 @@ describe('test/utils.test.js', () => {
     });
   });
 
-  describe('utils.destType()', () => {
+  describe('.destType()', () => {
     it('should throw error', () => {
       let err;
       try {
@@ -25,6 +25,55 @@ describe('test/utils.test.js', () => {
         err = e;
       }
       assert(err);
+    });
+  });
+
+  describe('.stripFileName()', () => {
+    it('should handle absolute path', () => {
+      assert(utils.stripFileName(0, '/', 'directory') === '/');
+      assert(utils.stripFileName(0, Buffer.from('/'), 'directory') === '/');
+      assert(utils.stripFileName(0, '/foo/', 'directory') === 'foo/');
+      assert(utils.stripFileName(0, '/foo/bar/', 'directory') === 'foo/bar/');
+      assert(utils.stripFileName(0, '/////foo/', 'directory') === 'foo/');
+
+      assert(utils.stripFileName(0, '/foo.txt', 'file') === 'foo.txt');
+      assert(utils.stripFileName(0, '///foo', 'file') === 'foo');
+    });
+
+    it('should handle relative path', () => {
+      assert(utils.stripFileName(0, 'foo/../bar/../../asdf/', 'directory') === 'asdf/');
+      assert(utils.stripFileName(0, '../../../../foo/', 'directory') === 'foo/');
+      assert(utils.stripFileName(0, '../../../foo/', 'directory') === 'foo/');
+      assert(utils.stripFileName(0, 'home/../../../../../foo', 'directory') === 'foo/');
+      assert(utils.stripFileName(0, '../../../../foo/../../../../', 'directory') === '/');
+      assert(utils.stripFileName(0, '/ok/../../../../../foo/bar/', 'directory') === 'foo/bar/');
+      assert(utils.stripFileName(0, '/////foo/../../../../foo/', 'directory') === 'foo/');
+
+      assert(utils.stripFileName(0, '/home/../../../foo.txt', 'file') === 'foo.txt');
+      assert(utils.stripFileName(0, '///../../../../foo', 'file') === 'foo');
+      assert(utils.stripFileName(0, '../../../../etc/hosts', 'file') === 'etc/hosts');
+    });
+
+    it('should replace \\', () => {
+      assert(utils.stripFileName(0, '\\', 'directory') === '/');
+      assert(utils.stripFileName(0, '/foo\\', 'directory') === 'foo/');
+      assert(utils.stripFileName(0, '/foo\\\\\\bar/', 'directory') === 'foo/bar/');
+      assert(utils.stripFileName(0, '//\\\\\\\\///foo/', 'directory') === 'foo/');
+
+      assert(utils.stripFileName(0, '\\foo.txt', 'file') === 'foo.txt');
+      assert(utils.stripFileName(0, '\\1\\2\\3\\4\\foo.txt', 'file') === '1/2/3/4/foo.txt');
+      assert(utils.stripFileName(0, '///\\\\\\foo', 'file') === 'foo');
+    });
+
+    it('should strip work', () => {
+      assert(utils.stripFileName(0, '/ok/../../../../../foo/bar/', 'directory') === 'foo/bar/');
+      assert(utils.stripFileName(1, '/ok/../../../../../foo/bar/', 'directory') === 'bar/');
+      assert(utils.stripFileName(2, '/ok/../../../../../foo/bar/', 'directory') === '/');
+
+      assert(utils.stripFileName(0, '\\1\\2\\3\\4\\foo.txt', 'file') === '1/2/3/4/foo.txt');
+      assert(utils.stripFileName(1, '\\1\\2\\3\\4\\foo.txt', 'file') === '2/3/4/foo.txt');
+      assert(utils.stripFileName(2, '\\1\\2\\3\\4\\foo.txt', 'file') === '3/4/foo.txt');
+      assert(utils.stripFileName(3, '\\1\\2\\3\\4\\foo.txt', 'file') === '4/foo.txt');
     });
   });
 });
