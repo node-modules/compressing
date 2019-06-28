939dbacc1f556ed204ce722ddde834be37fe9b8d
wanghx <whx89768@alibaba-inc.com>
2019-06-28 23:23:21 +0800

    feat: remove old version of node

--- test/ts.test.js
+++ test/ts.test.js
@@ -3,11 +3,11 @@
 const path = require('path');
 const coffee = require('coffee');
 
-describe('test/ts.test.js', async () => {
+describe('test/ts.test.js', () => {
   const tsconfigPath = path.resolve(__dirname, './fixtures/types/tsconfig.json');
 
-  it('should compile ts without error', async () => {
-    await coffee.fork(require.resolve('typescript/bin/tsc'), [ '-p', tsconfigPath ])
+  it('should compile ts without error', () => {
+    return coffee.fork(require.resolve('typescript/bin/tsc'), [ '-p', tsconfigPath ])
       .debug()
       .expect('code', 0)
       .end();
