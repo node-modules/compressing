b33d32b4614425416681259a9f7469f177ab6b7d
野声 <lijiacheng.ljc@antgroup.com>
2022-08-01 23:53:07 +0800

    chore(deps): remove deps

--- test/tgz/index.test.js
+++ test/tgz/index.test.js
@@ -8,7 +8,7 @@ const compressing = require('../..');
 const assert = require('power-assert');
 const rimraf = require('rimraf');
 const dircompare = require('dir-compare');
-const mkdirp = require('mz-modules/mkdirp');
+const mkdirp = require('mkdirp');
 
 describe('test/tgz/index.test.js', () => {
   let destDir;
@@ -17,32 +17,32 @@ describe('test/tgz/index.test.js', () => {
   });
 
   describe('tgz.compressFile()', () => {
-    it('tgz.compressFile(file, stream)', function* () {
+    it('tgz.compressFile(file, stream)', async () => {
       const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       console.log('dest', destFile);
       const fileStream = fs.createWriteStream(destFile);
-      yield compressing.tgz.compressFile(sourceFile, fileStream);
+      await compressing.tgz.compressFile(sourceFile, fileStream);
       assert(fs.existsSync(destFile));
     });
 
-    it('tgz.compressFile(file, stream, { relativePath })', function* () {
+    it('tgz.compressFile(file, stream, { relativePath })', async () => {
       const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       console.log('dest', destFile);
       const fileStream = fs.createWriteStream(destFile);
-      yield compressing.tgz.compressFile(sourceFile, fileStream, { relativePath: 'dd/dd.log' });
+      await compressing.tgz.compressFile(sourceFile, fileStream, { relativePath: 'dd/dd.log' });
       assert(fs.existsSync(destFile));
       // TODO 检查 uncompress 之后 relativePath
     });
 
-    it('tgz.compressFile(file, stream) should error if file not exist', function* () {
+    it('tgz.compressFile(file, stream) should error if file not exist', async () => {
       const sourceFile = path.join(__dirname, '..', 'fixtures', 'not-exist.log');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       const fileStream = fs.createWriteStream(destFile);
       let err;
       try {
-        yield compressing.tgz.compressFile(sourceFile, fileStream, { relativePath: 'dd/dd.log' });
+        await compressing.tgz.compressFile(sourceFile, fileStream, { relativePath: 'dd/dd.log' });
       } catch (e) {
         err = e;
       }
@@ -50,32 +50,32 @@ describe('test/tgz/index.test.js', () => {
       assert(err.message.indexOf('no such file or directory') > -1);
     });
 
-    it('tgz.compressFile(file, destStream) should error if destStream emit error', function* () {
+    it('tgz.compressFile(file, destStream) should error if destStream emit error', async () => {
       const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       const fileStream = fs.createWriteStream(destFile);
       setImmediate(() => fileStream.emit('error', new Error('xx')));
       let err;
       try {
-        yield compressing.tgz.compressFile(sourceFile, fileStream);
+        await compressing.tgz.compressFile(sourceFile, fileStream);
       } catch (e) {
         err = e;
       }
       assert(err && err.message === 'xx');
     });
 
-    it('should keep stat mode', function* () {
+    it('should keep stat mode', async () => {
       const sourceFile = path.join(__dirname, '..', 'fixtures/xxx/bin');
       const originStat = fs.statSync(sourceFile);
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tar');
       console.log('dest', destFile);
       const fileStream = fs.createWriteStream(destFile);
-      yield compressing.tgz.compressFile(sourceFile, fileStream);
+      await compressing.tgz.compressFile(sourceFile, fileStream);
       assert(fs.existsSync(destFile));
 
       destDir = path.join(os.tmpdir(), uuid.v4());
-      yield mkdirp(destDir);
-      yield compressing.tgz.uncompress(destFile, destDir);
+      await mkdirp(destDir);
+      await compressing.tgz.uncompress(destFile, destDir);
       const stat = fs.statSync(path.join(destDir, 'bin'));
       assert(stat.mode);
       assert(originStat.mode);
@@ -85,41 +85,41 @@ describe('test/tgz/index.test.js', () => {
   });
 
   describe('tgz.compressDir()', () => {
-    it('tgz.compressDir(dir, destFile)', function* () {
+    it('tgz.compressDir(dir, destFile)', async () => {
       const sourceDir = path.join(__dirname, '..', 'fixtures');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       console.log('dest', destFile);
-      yield compressing.tgz.compressDir(sourceDir, destFile);
+      await compressing.tgz.compressDir(sourceDir, destFile);
       assert(fs.existsSync(destFile));
     });
 
-    it('tgz.compressDir(dir, destStream)', function* () {
-      const sourceDir = path.join(__dirname, '..', 'fixtures');
-      const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
-      const destStream = fs.createWriteStream(destFile);
-      console.log('dest', destFile);
-      yield compressing.tgz.compressDir(sourceDir, destStream);
-      assert(fs.existsSync(destFile));
-    });
-
-    it('tgz.compressDir(dir, destStream, { ignoreBase: true })', function* () {
+    it('tgz.compressDir(dir, destStream)', async () => {
       const sourceDir = path.join(__dirname, '..', 'fixtures');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       const destStream = fs.createWriteStream(destFile);
       console.log('dest', destFile);
-      yield compressing.tgz.compressDir(sourceDir, destStream, { ignoreBase: true });
+      await compressing.tgz.compressDir(sourceDir, destStream);
       assert(fs.existsSync(destFile));
     });
 
-    it('tgz.compressDir(dir, destStream) should return promise', function* () {
+    it('tgz.compressDir(dir, destStream, { ignoreBase: true })', async () => {
+      const sourceDir = path.join(__dirname, '..', 'fixtures');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
+      const destStream = fs.createWriteStream(destFile);
+      console.log('dest', destFile);
+      await compressing.tgz.compressDir(sourceDir, destStream, { ignoreBase: true });
+      assert(fs.existsSync(destFile));
+    });
+
+    it('tgz.compressDir(dir, destStream) should return promise', async () => {
       const sourceDir = path.join(__dirname, '..', 'fixtures');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       console.log('dest', destFile);
-      yield compressing.tgz.compressDir(sourceDir, destFile);
+      await compressing.tgz.compressDir(sourceDir, destFile);
       assert(fs.existsSync(destFile));
     });
 
-    it('tgz.compressDir(dir, destStream) should reject when destStream emit error', function* () {
+    it('tgz.compressDir(dir, destStream) should reject when destStream emit error', async () => {
       const sourceDir = path.join(__dirname, '..', 'fixtures');
       const destFile = path.join(os.tmpdir(), uuid.v4() + '.tgz');
       const destStream = fs.createWriteStream(destFile);
@@ -128,7 +128,7 @@ describe('test/tgz/index.test.js', () => {
       });
       let err;
       try {
-        yield compressing.tgz.compressDir(sourceDir, destStream);
+        await compressing.tgz.compressDir(sourceDir, destStream);
       } catch (e) {
         err = e;
       }
@@ -136,12 +136,12 @@ describe('test/tgz/index.test.js', () => {
       assert(err.message === 'xxx');
     });
 
-    it('tgz.compressDir(dir, destStream) should reject when destFile cannot be created', function* () {
+    it('tgz.compressDir(dir, destStream) should reject when destFile cannot be created', async () => {
       const sourceDir = path.join(__dirname, '..', 'fixtures');
       const destFile = path.join('/permision-deny');
       let err;
       try {
-        yield compressing.tgz.compressDir(sourceDir, destFile);
+        await compressing.tgz.compressDir(sourceDir, destFile);
       } catch (e) {
         err = e;
       }
@@ -152,11 +152,11 @@ describe('test/tgz/index.test.js', () => {
   });
 
   describe('tgz.uncompress()', () => {
-    it('tgz.uncompress(sourceFile, destDir)', function* () {
+    it('tgz.uncompress(sourceFile, destDir)', async () => {
       const sourceFile = path.join(__dirname, '..', 'fixtures', 'xxx.tgz');
       destDir = path.join(os.tmpdir(), uuid.v4());
       const originalDir = path.join(__dirname, '..', 'fixtures', 'xxx');
-      yield compressing.tgz.uncompress(sourceFile, destDir);
+      await compressing.tgz.uncompress(sourceFile, destDir);
 
       assert(fs.statSync(path.join(destDir, 'xxx/bar.txt')).size === 7);
       assert(fs.statSync(path.join(destDir, 'xxx/foo')).size === 3);
@@ -176,11 +176,11 @@ describe('test/tgz/index.test.js', () => {
       assert(originStat.mode);
     });
 
-    it('tgz.uncompress(sourceStream, destDir)', function* () {
+    it('tgz.uncompress(sourceStream, destDir)', async () => {
       const sourceStream = fs.createReadStream(path.join(__dirname, '..', 'fixtures', 'xxx.tgz'));
       destDir = path.join(os.tmpdir(), uuid.v4());
       const originalDir = path.join(__dirname, '..', 'fixtures', 'xxx');
-      yield compressing.tgz.uncompress(sourceStream, destDir);
+      await compressing.tgz.uncompress(sourceStream, destDir);
       const res = dircompare.compareSync(originalDir, path.join(destDir, 'xxx'));
       assert(res.distinct === 0);
       assert(res.equal === 5);
@@ -188,11 +188,11 @@ describe('test/tgz/index.test.js', () => {
       assert(res.totalDirs === 1);
     });
 
-    it('tgz.uncompress(sourceBuffer, destDir)', function* () {
+    it('tgz.uncompress(sourceBuffer, destDir)', async () => {
       const sourceBuffer = fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'xxx.tgz'));
       destDir = path.join(os.tmpdir(), uuid.v4());
       const originalDir = path.join(__dirname, '..', 'fixtures', 'xxx');
-      yield compressing.tgz.uncompress(sourceBuffer, destDir);
+      await compressing.tgz.uncompress(sourceBuffer, destDir);
       const res = dircompare.compareSync(originalDir, path.join(destDir, 'xxx'));
       assert(res.distinct === 0);
       assert(res.equal === 5);
