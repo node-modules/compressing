2dfc38fee06e77fd22bb94a423260e186f0e3e77
fengmk2 <fengmk2@gmail.com>
2025-08-12 17:24:09 +0800

    feat: add XZ compression and decompression support
    - Add XZ compression module with FileStream and UncompressStream classes
    - Implement compressFile() and uncompress() functions
    - Add TypeScript definitions for XZ module
    - Add comprehensive unit tests for all XZ functionality
    - Support compression levels 1-9 and multi-threading options
    - Fixes #96

---
+++ test/xz/index.test.js
@@ -0,0 +1,151 @@
+'use strict';
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const uuid = require('uuid');
+const compressing = require('../..');
+const assert = require('assert');
+const isWindows = os.platform() === 'win32';
+
+describe('test/xz/index.test.js', () => {
+  describe('xz.compressFile()', () => {
+    it('xz.compressFile(file, stream)', async () => {
+      const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log.xz');
+      // console.log('destFile', destFile);
+      const fileStream = fs.createWriteStream(destFile);
+      await compressing.xz.compressFile(sourceFile, fileStream);
+      assert(fs.existsSync(destFile));
+    });
+
+    it('xz.compressFile(file, destStream) should error if destStream emit error', async () => {
+      const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.xz');
+      const fileStream = fs.createWriteStream(destFile);
+      setImmediate(() => fileStream.emit('error', new Error('xx')));
+
+      let err;
+      try {
+        await compressing.xz.compressFile(sourceFile, fileStream);
+      } catch (e) {
+        err = e;
+      }
+      assert(err && err.message === 'xx');
+    });
+
+    it('xz.compressFile(buffer, stream)', async () => {
+      const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const sourceBuffer = fs.readFileSync(sourceFile);
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log.xz');
+      // console.log('destFile', destFile);
+      const fileStream = fs.createWriteStream(destFile);
+      await compressing.xz.compressFile(sourceBuffer, fileStream);
+      assert(fs.existsSync(destFile));
+    });
+
+    it('xz.compressFile(sourceStream, destStream)', async () => {
+      const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const sourceStream = fs.createReadStream(sourceFile);
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log.xz');
+      // console.log('destFile', destFile);
+      const fileStream = fs.createWriteStream(destFile);
+      await compressing.xz.compressFile(sourceStream, fileStream);
+      assert(fs.existsSync(destFile));
+    });
+
+    it('xz.compressFile(file, stream) with custom level', async () => {
+      const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log.xz');
+      const fileStream = fs.createWriteStream(destFile);
+      await compressing.xz.compressFile(sourceFile, fileStream, { level: 9 });
+      assert(fs.existsSync(destFile));
+    });
+  });
+
+  describe('xz.uncompress()', () => {
+    let compressedFile;
+    
+    before(async () => {
+      // Create a compressed file for testing
+      const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      compressedFile = path.join(os.tmpdir(), 'test-xx.log.xz');
+      const fileStream = fs.createWriteStream(compressedFile);
+      await compressing.xz.compressFile(sourceFile, fileStream);
+    });
+
+    it('xz.uncompress(sourceFile, destStream)', async () => {
+      const originalFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+      const fileStream = fs.createWriteStream(destFile);
+      await compressing.xz.uncompress(compressedFile, fileStream);
+      assert(fs.existsSync(destFile));
+      if (!isWindows) {
+        // EOL not equal to linux
+        assert(fs.readFileSync(destFile, 'utf8') === fs.readFileSync(originalFile, 'utf8'));
+      }
+    });
+
+    it('xz.uncompress(sourceStream, destStream)', async () => {
+      const sourceStream = fs.createReadStream(compressedFile);
+      const originalFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+      const fileStream = fs.createWriteStream(destFile);
+      await compressing.xz.uncompress(sourceStream, fileStream);
+      assert(fs.existsSync(destFile));
+      if (!isWindows) {
+        // EOL not equal to linux
+        assert(fs.readFileSync(destFile, 'utf8') === fs.readFileSync(originalFile, 'utf8'));
+      }
+    });
+
+    it('xz.uncompress(sourceStream, destFile)', async () => {
+      const sourceStream = fs.createReadStream(compressedFile);
+      const originalFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+      await compressing.xz.uncompress(sourceStream, destFile);
+      assert(fs.existsSync(destFile));
+      if (!isWindows) {
+        // EOL not equal to linux
+        assert(fs.readFileSync(destFile, 'utf8') === fs.readFileSync(originalFile, 'utf8'));
+      }
+    });
+
+    it('xz.uncompress(sourceFile, destFile)', async () => {
+      const originalFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+      await compressing.xz.uncompress(compressedFile, destFile);
+      assert(fs.existsSync(destFile));
+      if (!isWindows) {
+        // EOL not equal to linux
+        assert(fs.readFileSync(destFile, 'utf8') === fs.readFileSync(originalFile, 'utf8'));
+      }
+    });
+
+    it('xz.uncompress(buffer, destFile)', async () => {
+      const sourceBuffer = fs.readFileSync(compressedFile);
+      const originalFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+      await compressing.xz.uncompress(sourceBuffer, destFile);
+      assert(fs.existsSync(destFile));
+      if (!isWindows) {
+        // EOL not equal to linux
+        assert(fs.readFileSync(destFile, 'utf8') === fs.readFileSync(originalFile, 'utf8'));
+      }
+    });
+
+    it('xz.uncompress should error if destStream emit error', async () => {
+      const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+      const fileStream = fs.createWriteStream(destFile);
+      setImmediate(() => fileStream.emit('error', new Error('write error')));
+
+      let err;
+      try {
+        await compressing.xz.uncompress(compressedFile, fileStream);
+      } catch (e) {
+        err = e;
+      }
+      assert(err && err.message === 'write error');
+    });
+  });
+});
---
+++ test/xz/uncompress_stream.test.js
@@ -0,0 +1,150 @@
+const fs = require('fs');
+const mm = require('mm');
+const os = require('os');
+const uuid = require('uuid');
+const path = require('path');
+const assert = require('assert');
+const { pipeline: pump } = require('stream');
+const streamifier = require('streamifier');
+const compressing = require('../..');
+
+const originalFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+const isWindows = os.platform() === 'win32';
+
+// Helper function to create XZ compressed file for testing
+function createXzFile() {
+  const sourceFile = path.join(__dirname, '..', 'fixtures', 'xx.log');
+  const destFile = path.join(os.tmpdir(), 'test-xx.log.xz');
+  
+  return new Promise((resolve, reject) => {
+    const sourceStream = fs.createReadStream(sourceFile);
+    const xzStream = new compressing.xz.FileStream();
+    const destStream = fs.createWriteStream(destFile);
+    
+    pump(sourceStream, xzStream, destStream, err => {
+      if (err) reject(err);
+      else resolve(destFile);
+    });
+  });
+}
+
+describe('test/xz/uncompress_stream.test.js', () => {
+  let sourceFile;
+  
+  before(async () => {
+    sourceFile = await createXzFile();
+  });
+  
+  afterEach(mm.restore);
+
+  it('should be transform stream', done => {
+    const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+
+    const sourceStream = fs.createReadStream(sourceFile);
+    const uncompressStream = new compressing.xz.UncompressStream();
+    const destStream = fs.createWriteStream(destFile);
+    pump(sourceStream, uncompressStream, destStream, err => {
+      assert(!err);
+      assert(fs.existsSync(destFile));
+      const originalFileBuffer = fs.readFileSync(originalFile);
+      const destFileBuffer = fs.readFileSync(destFile);
+      assert.equal(destFileBuffer.length, originalFileBuffer.length);
+      if (!isWindows) {
+        // EOL not equal to linux
+        assert.equal(destFileBuffer.toString('utf8'), originalFileBuffer.toString('utf8'));
+      }
+      done();
+    });
+  });
+
+  it('should uncompress according to file path', done => {
+    const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+
+    const uncompressStream = new compressing.xz.UncompressStream({ source: sourceFile });
+    const destStream = fs.createWriteStream(destFile);
+    pump(uncompressStream, destStream, err => {
+      assert(!err);
+      assert(fs.existsSync(destFile));
+      const originalFileBuffer = fs.readFileSync(originalFile);
+      const destFileBuffer = fs.readFileSync(destFile);
+      assert.equal(destFileBuffer.length, originalFileBuffer.length);
+      if (!isWindows) {
+        assert.equal(destFileBuffer.toString('utf8'), originalFileBuffer.toString('utf8'));
+      }
+      done();
+    });
+  });
+
+  it('should uncompress buffer', done => {
+    const sourceBuffer = fs.readFileSync(sourceFile);
+    const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+
+    const destStream = fs.createWriteStream(destFile);
+    const uncompressStream = new compressing.xz.UncompressStream({ source: sourceBuffer });
+    pump(uncompressStream, destStream, err => {
+      assert(!err);
+      assert(fs.existsSync(destFile));
+      const originalFileBuffer = fs.readFileSync(originalFile);
+      const destFileBuffer = fs.readFileSync(destFile);
+      assert.equal(destFileBuffer.length, originalFileBuffer.length);
+      if (!isWindows) {
+        assert.equal(destFileBuffer.toString('utf8'), originalFileBuffer.toString('utf8'));
+      }
+      done();
+    });
+  });
+
+  it('should uncompress stream', done => {
+    const sourceStream = fs.createReadStream(sourceFile);
+    const destFile = path.join(os.tmpdir(), uuid.v4() + '.log');
+
+    const destStream = fs.createWriteStream(destFile);
+    const uncompressStream = new compressing.xz.UncompressStream({ source: sourceStream });
+    pump(uncompressStream, destStream, err => {
+      assert(!err);
+      assert(fs.existsSync(destFile));
+      const originalFileBuffer = fs.readFileSync(originalFile);
+      const destFileBuffer = fs.readFileSync(destFile);
+      assert.equal(destFileBuffer.length, originalFileBuffer.length);
+      if (!isWindows) {
+        assert.equal(destFileBuffer.toString('utf8'), originalFileBuffer.toString('utf8'));
+      }
+      done();
+    });
+  });
+
+  it('should emit error if sourceFile does not exit', done => {
+    const sourceFile = 'file-not-exist';
+    const uncompressStream = new compressing.xz.UncompressStream({ source: sourceFile });
+    uncompressStream.on('error', err => {
+      assert(err);
+      done();
+    });
+  });
+
+  it('should emit error if sourceStream emit error', done => {
+    const sourceFile = 'file-not-exist';
+    const sourceStream = fs.createReadStream(sourceFile);
+    const uncompressStream = new compressing.xz.UncompressStream({ source: sourceStream });
+    uncompressStream.on('error', err => {
+      assert(err && err.code === 'ENOENT');
+      done();
+    });
+  });
+
+  it('should emit error if stream created by streamifier.createReadStream emit error', done => {
+    const original = streamifier.createReadStream;
+    mm(streamifier, 'createReadStream', function() {
+      const result = original.apply(streamifier, arguments);
+      setImmediate(() => result.emit('error', 'mockError'));
+      return result;
+    });
+    const sourceBuffer = fs.readFileSync(sourceFile);
+    const uncompressStream = new compressing.xz.UncompressStream({ source: sourceBuffer });
+    uncompressStream.on('error', err => {
+      assert(err === 'mockError');
+      done();
+    });
+  });
+
+});
